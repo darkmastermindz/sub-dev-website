e684b14e810bc08452cbbf356c9b80d8e8e53543
Hansel Wei <darkmastermindz@gmail.com>
2017-11-14 23:20:56 -0500

    Updated Website with fun stuff

--- js/animations.js
+++ js/animations.js
@@ -1,45 +1,11 @@
-var pierreImg = new image();
-pierreImg.src = "images/sprS_walkrighttest";
+//Sidebar Navigation Animations
+$(document).ready(function() {
+  $('#sidebar-btn').click(function() {
+    $('#sidebar').toggleClass('visible');
+  });
+});
 
-function sprite (options) {
-  var spr = {};
-
-  spr.context = options.context;
-  spr.width = options.width;
-  spr.height = options.height;
-  spr.image = options.image;
-
-  return this;
-
-}
-
-var canvas = document.getElementById("pierreAnimation");
-canvas.width = 100;
-canvas.height = 100;
-
-var pierre = sprite({
-  content: canvas.getContext("2d"),
-  width: 100,
-  height: 100,
-  image: pierreImg;
-})
-
-function sprite (options) {
-    spr.render = function () {
-
-        // Draw the animation
-        spr.context.drawImage(
-           spr.image,
-           0,
-           0,
-           spr.width,
-           that.height,
-           0,
-           0,
-           spr.width,
-           spr.height);
-    };
-
-}
-
-pierre.render();
+$("#chest").click( function() {
+    var value = parseInt($('#increment').text(), 10) + 1;
+    $("#increment").text(value);
+});
---
+++ js/email.js
@@ -0,0 +1,14 @@
+//
+// TODO: Connect to Mailchimp API
+//
+/*
+$('.email-form').on('submit', function () {
+var fname = document.getElementById('fname').value;
+var lname = document.getElementById('lname').value;
+var email = document.getElementById('email').value;
+var message = document.getElementById('message').value;
+
+alert("TODO: This is a DOM test for 2300 CBE. An email will be sent with the following parameters:"
+      + "\nFirst Name " +fname+"\nLast Name: "+lname+"\nMessage: "+message);
+}
+*/
