9261ce3eba16643d4f2c0c8fcbfe035f4e4d719c
Hansel Wei <darkmastermindz@gmail.com>
2017-11-15 02:39:34 -0500

    fixing port issue with heroku

--- Server.js
+++ Server.js
@@ -2,6 +2,7 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 var path = __dirname + '/views/';
+var port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/public/'));
 app.listen(3000);
